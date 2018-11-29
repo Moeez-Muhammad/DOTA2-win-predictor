cf026f9c532d508c427200d7a38c7951484b497c
Moeez Muhammad <mmoeez48@gmail.com>
2018-11-28 21:17:40 -0500

    Revamp scraper.py to use the offical DOTA2 API from Valve and have it finally work!!!

--- scraper.py
+++ scraper.py
@@ -1,56 +1,46 @@
-import requests
-import simplejson as json
-#4152438307
-def get_raw_data(howManyMatches, highestMatchID=""):
-    '''
-    ///MUST READ TO UNDERSTAND CODE AND COMMENTS///
+import json
+import dota2api
 
-    Opendota's API works like this:
-        * /publicmatches?mmr_descending=1 gets a random sample of public matches with a high skill rating, sorting ascending by matchid
-        * Example list of matchids: {100..200}
-        * /publicmatches?mmr_descending=1&less_than_match_id=100 gets a random sample of public matches with a high skill rating, sorting descending by matchid, where all matchid < less_than_match_id
-        * So, if we set less_than_match_id to blank, we get a random sample sorted matchid ascending from a to b
-        * Then, we take a, and get a random sample of matchids less than a from a to c where c < a
-        * We then take c and keep recursively doing that, going lower and lower in matchids until the required number of matches has been met
-    '''
-    rawMatches = [] # Collective list of all matches to be used
-    while len(rawMatches) < howManyMatches: # Check if we have the required number of matches
-        parameters = {'mmr_descending': 1, 'less_than_match_id': highestMatchID} # Sort the matches in decreasing skill order and lower than a specific matchid
-        response = requests.get('https://api.opendota.com/api/publicMatches', params=parameters) # GET matches data from api.opendota.com
-        matchSet = json.loads(response.content) # Get the json of the response data
-        rawMatches = rawMatches + matchSet # Concatenate new data with existing data
-        highestMatchID = matchSet[0]["match_id"] # Get new highest matchid from the first match
-        with open('raw_matches.json', 'w') as outfile:
-            json.dump(rawMatches, outfile)
-    return rawMatches
+radiant_team_slots = [0, 1, 2, 3, 4]
+dire_team_slots = [128, 129, 130, 131, 132]
+api = None
 
-def parse_data(rawMatches):
-    '''
-        This function takes the rawMatches, iterates through it, and gets the match_id, radiant_win, radiant_team and dire_team variables
-        It then stores them in a list of dictionaries where one dictionary is one match
-    '''
+def initialise():
+    with open('apikey', 'r') as apikey_file:
+        apikey = str(apikey_file.read())
+    global api
+    api = dota2api.Initialise(apikey)
+
+def get_raw_data(matches_requested):
+    match_ids = []
+    rawMatches = api.get_match_history(game_mode=1, skill=3, matches_requested=matches_requested)["matches"] # Collective list of all matches to be used
+    for match in rawMatches:
+        match_ids.append(match["match_id"])
+    return match_ids
+
+def parse_data(match_ids):
+    rawMatches = []
     matches = []
+    for match_id in match_ids:
+        rawMatches.append(api.get_match_details(match_id=match_id))
     for match in rawMatches:
         newMatch = {}
-        newMatch["radiant_win"], newMatch["radiant_team"], newMatch["dire_team"] = match["radiant_win"], match["radiant_team"], match["dire_team"]
+        radiant_team = []
+        dire_team = []
+        players = match["players"]
+        for player in players:
+            if player["player_slot"] in radiant_team_slots:
+                radiant_team.append(player["hero_id"])
+            elif player["player_slot"] in dire_team_slots:
+                dire_team.append(player["hero_id"])
+
+        newMatch["radiant_win"], newMatch["radiant_team"], newMatch["dire_team"] = match["radiant_win"], radiant_team, dire_team
         matches.append(newMatch)
-    with open('matches.json', 'w') as outfile:
+    with open('matches.json', 'a') as outfile:
         json.dump(matches, outfile)
     return matches
 
 
-
-rawMatches = get_raw_data(200) # highestMatchID is blank because the current matchids of the current matches could change
+initialise()
+rawMatches = get_raw_data(matches_requested=1000) # highestMatchID is blank because the current matchids of the current matches could change
 print(parse_data(rawMatches))
-
-# We need to get these main things: matchid, who wins, and what heroes were in the matchid
-# We gather first 100 through standard GET and then get the rest using less than this match id parameter
-# We need a list of dictionaries where one dictionary is one match
-# We also need to get a list of hero ids and their corresponding hero names
-# Divide file into two functions: one to get all the data and then another to parse
-# One function could be made to get hero data but that won't be ran in the final code as we can store the results
-# Code can be in the final file however, for documentation purposes
-# We could make the parse function run as the data comes in, adding each match to the matches list
-# However that makes it run slower
-
-#matches = []

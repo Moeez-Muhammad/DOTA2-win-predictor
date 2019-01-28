f8220d6083034b729f4b1754b028d67065e2d938
Moeez Muhammad <mmoeez48@gmail.com>
2019-01-27 21:22:22 -0500

    testing again?

--- scraper.py
+++ scraper.py
@@ -8,3 +8,4 @@ matches = api.get_more_matches(matches_requested=3000000, min_mmr=0)
 parsed_matches = api.parse_matches_for_ml(matches, file_outputs=["matches.json", "results.json"], append=False)
 print(json.dumps(parsed_matches, indent=4, sort_keys=True))
 #test
+#test

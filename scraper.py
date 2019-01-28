e028803dffe4fbd542c9442a4aac923c99b658b9
Moeez Muhammad <mmoeez48@gmail.com>
2019-01-27 21:21:37 -0500

    test

--- scraper.py
+++ scraper.py
@@ -7,3 +7,4 @@ api.generate_hero_dict()
 matches = api.get_more_matches(matches_requested=3000000, min_mmr=0)
 parsed_matches = api.parse_matches_for_ml(matches, file_outputs=["matches.json", "results.json"], append=False)
 print(json.dumps(parsed_matches, indent=4, sort_keys=True))
+#test
